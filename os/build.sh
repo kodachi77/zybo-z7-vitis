62234b2fb74b65dc3abbb16056068600d8fde4f6
kodachi77 <12465849+kodachi77@users.noreply.github.com>
2025-01-12 10:02:12 -0500

    minor changes to speed up boot process (DMA)

--- os/build.sh
+++ os/build.sh
@@ -2,15 +2,16 @@
 
 sudo -v
 
-#petalinux-config --get-hw-description=../hw/hw_handoff/
-#petalinux-config -c rootfs
-#petalinux-config -c kernel
-#petaliux-config -c u-boot
+petalinux-config --get-hw-description=../hw/hw_handoff/
+petalinux-config -c rootfs
+petalinux-config -c kernel
+petaliux-config -c u-boot
 
-#petalinux-build
+petalinux-build
 
 petalinux-build --sdk
-petalinux-package --boot --fsbl images/linux/zynq_fsbl.elf --fpga images/linux/system.bit --uboot --force
+petalinux-package --boot --fsbl images/linux/zynq_fsbl.elf --fpga images/linux/system.bit --u-boot --boot-device sd --force
+#petalinux-package --boot --bif images/linux/BOOT.bif --boot-device sd --force
 petalinux-package --sysroot
 
-sudo chown $USER:$USER /home/kodachi77/project/zybo-z7-vitis/os/images/linux/sdk/sysroots/cortexa9t2hf-neon-xilinx-linux-gnueabi/usr/bin/sudo
+sudo chown $USER:$USER images/linux/sdk/sysroots/cortexa9t2hf-neon-xilinx-linux-gnueabi/usr/bin/sudo
